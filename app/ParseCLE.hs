d7109bf6755309e94ae1f62b9a68f9929b597dbb
mlevatich <ml4553@columbia.edu>
2021-05-06 18:34:49 -0400

    sample JSON parse with Aeson

--- app/ParseCLE.hs
+++ app/ParseCLE.hs
@@ -1,7 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module ParseCLE where
 
 import Data.Aeson
+import GHC.Generics
+
+data Person = Person {
+  name :: String
+, age :: Int
+} deriving (Generic, Show)
+
+instance ToJSON Person where
+instance FromJSON Person where
 
 main :: IO ()
 main = do
-  putStrLn "HELLO WORLD"
+  print $ encode (Person {name = "Joe", age = 12})
