a12f617d4fe7266190ea093f7e504ca00cb9961b
mlevatich <ml4553@columbia.edu>
2021-05-13 15:29:52 -0400

    ParseCLE: full clemap validation for individual files

--- app/ParseCLE.hs
+++ app/ParseCLE.hs
@@ -2,12 +2,15 @@
 
 module ParseCLE where
 
-import Data.Aeson
+import Data.Aeson hiding (json)
 import GHC.Generics
 import System.IO
 import System.Environment
 import Data.ByteString.Lazy.UTF8 (fromString)
+import Data.List
 import qualified Data.Map.Strict as M
+
+type CLEMap = [CLE]
 
 data CLE = CLE {
   label :: String
@@ -23,16 +26,21 @@ data CDF = CDF {
   remotelevel :: String
 , direction :: String
 , guarddirective :: GD
-, argtaints :: Maybe [[String]]
-, codtaints :: Maybe [[String]]
-, rettaints :: Maybe [[String]]
+, argtaints :: Taints
+, codtaints :: Taints
+, rettaints :: Taints
 } deriving (Generic, Show)
+
+type Taints = Maybe [[String]]
 
 data GD = GD {
   operation :: String
 , gapstag :: Maybe [Int]
 , oneway :: Maybe Bool
 } deriving (Generic, Show)
+
+type StringTable = (M.Map String Int)
+type Error = String
 
 instance FromJSON CLE where
   parseJSON = genericParseJSON o
@@ -44,8 +52,77 @@ instance FromJSON CLEJSON where
 instance FromJSON CDF where
 instance FromJSON GD where
 
-validate :: [CLE] -> Maybe String
-validate _ = Nothing
+colors :: [String]
+colors = ["green", "orange"]
+
+ops :: [String]
+ops = ["allow", "deny", "redact"]
+
+directions :: [String]
+directions = ["ingress", "egress"]
+
+firstError :: [Maybe Error] -> Maybe Error
+firstError [] = Nothing
+firstError (x:xs) = case x of
+  Nothing -> firstError xs
+  e -> e
+
+gdValidate :: StringTable -> GD -> Maybe Error
+gdValidate _ (GD op _ _) =
+  if op `elem` ops
+  then Nothing
+  else Just $ "unknown operation: '" ++ op ++ "'"
+
+taintsValidate :: StringTable -> Taints -> Maybe Error
+taintsValidate _ Nothing = Nothing
+taintsValidate tbl (Just tss) = firstError $ map (taintValidate tbl) tss
+  where
+    taintValidate _ [] = Nothing
+    taintValidate tbl' (t:ts) =
+      if M.member t tbl'
+      then taintValidate tbl' ts
+      else Just $ "taint label does not exist: '" ++ t ++ "'"
+
+cdfValidate :: StringTable -> CDF -> Maybe Error
+cdfValidate tbl (CDF lvl dir gd argt codt rett) = firstError all_errs
+  where
+    all_errs = [lvl_err, dir_err, gd_err, argt_err, codt_err, rett_err]
+    gd_err = gdValidate tbl gd
+    argt_err = taintsValidate tbl argt
+    codt_err = taintsValidate tbl codt
+    rett_err = taintsValidate tbl rett
+    lvl_err =
+      if lvl `elem` colors
+      then Nothing
+      else Just $ "unknown level: '" ++ lvl ++ "'"
+    dir_err =
+      if dir `elem` directions
+      then Nothing
+      else Just $ "unknown direction: '" ++ dir ++ "'"
+
+jsonValidate :: StringTable -> CLEJSON -> Maybe Error
+jsonValidate tbl (CLEJSON cdfs lvl) =
+  if lvl `elem` colors
+  then (case cdfs of
+    Just cdfs' -> firstError $ map (cdfValidate tbl) cdfs'
+    _ -> Nothing)
+  else Just $ "unknown level: '" ++ lvl ++ "'"
+
+cleValidate :: StringTable -> CLE -> Maybe Error
+cleValidate tbl (CLE l js) = case jsonValidate tbl js of
+  Just e -> Just $ "in CLE '" ++ l ++ "': " ++ e
+  _ -> Nothing
+
+clemapValidate :: [CLE] -> Either Error StringTable
+clemapValidate cles =
+  if (length $ nub lbls) == length lbls
+  then (case firstError (map (cleValidate tbl) cles) of
+    Just e -> Left e
+    Nothing -> Right tbl)
+  else (Left "duplicate cle-label")
+  where
+    lbls = map label cles
+    tbl = M.fromList $ zip lbls [0..]
 
 readCLE :: String -> IO [CLE]
 readCLE f = do
@@ -58,11 +135,15 @@ readCLE f = do
 
 main :: IO ()
 main = do
+  let ex_args = "green_clemap.json orange_clemap.json refactored_clemap.json"
   args <- getArgs
-  case length args of
-    0 -> putStrLn "error: please provide a CLE json file to parse"
-    _ -> do
-      cle <- readCLE $ head args
-      case validate cle of
-        Nothing -> putStrLn $ "input CLE json is valid!"
-        Just e  -> putStrLn $ "error: " ++ e
+  case args of
+    fs@[_, _, _] -> do
+      clemaps@[_, _, _] <- mapM readCLE fs
+      mapM_ check $ zip clemaps fs
+      -- TODO: inter-file validation
+      where
+        check (cles, fname) = case clemapValidate cles of
+          Right _ -> putStrLn $ fname ++ " is a well-formed CLE map."
+          Left e  -> putStrLn $ fname ++ ": error: " ++ e ++ "."
+    _ -> putStrLn $ "usage: parseCLE " ++ ex_args
